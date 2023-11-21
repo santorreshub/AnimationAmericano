5529f0eceb15243c8292f8fb007554496c7676e2
Santiago Torres Alvarez <santiagot2000@gmail.com>
2023-11-21 13:52:33 +0100

    Initial Commit

---
+++ AnimationAmericano/AnimationAmericanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAmericanoApp.swift
+//  AnimationAmericano
+//
+//  Created by Santiago Torres Alvarez on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAmericanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationAmericano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationAmericano
+//
+//  Created by Santiago Torres Alvarez on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
